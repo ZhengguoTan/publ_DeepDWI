18240cf02820724f23e42260bc7a244a0c53a1d6
Zhengguo Tan <zhengguo.tan@gmail.com>
2025-11-17 10:11:21 -0500

    sub3

---
+++ 02_mrm/sub3/manuscript/bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r main.zip figures/ empty.eps MRM-AMA.bbl MRM-AMA.bib MRM-AMA.bst MRM-AMA.tex MRM-AMA-clean.tex MRM.cls NJDnatbib.sty Orcidlogo.eps Orcidlogo-eps-converted-to.pdf widetext.sty
